a3e1a3b1a2480f4f9d7567f42c29617d610cceae
alyhenr <alyssonhra99@gmail.com>
2023-08-14 17:17:29 -0300

    chore: url front end deploy cors

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cookieParser());
 app.use(cors({
-    credentials: true, origin: 'https://projeto18-freela-front-4rtt.vercel.app/'
+    credentials: true, origin: 'https://projeto18-freela-front-4rtt-mmc9agtfj-alyhenr.vercel.app/'
 }))
 app.use(express.json());
 
